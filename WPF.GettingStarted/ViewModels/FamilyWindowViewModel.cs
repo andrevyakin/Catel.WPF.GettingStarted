d32ab57c790f7a061ad4f4a232b0dbeeb2255c21
Ревякин Андрей Владимирович <andrevyakin@hotmail.com>
2018-08-26 17:25:26 +0300

    Перевожу коментарии

--- WPF.GettingStarted/ViewModels/FamilyWindowViewModel.cs
+++ WPF.GettingStarted/ViewModels/FamilyWindowViewModel.cs
@@ -12,8 +12,8 @@ namespace WPF.GettingStarted.ViewModels
 
     public class FamilyWindowViewModel : ViewModelBase
     {
-        private readonly IUIVisualizerService _uiVisualizerService;
-        private readonly IMessageService _messageService;
+        private readonly IUIVisualizerService uiVisualizerService;
+        private readonly IMessageService messageService;
 
         public FamilyWindowViewModel(Family family, IUIVisualizerService uiVisualizerService, IMessageService messageService)
         {
@@ -22,8 +22,8 @@ namespace WPF.GettingStarted.ViewModels
             Argument.IsNotNull(() => messageService);
 
             Family = family;
-            _uiVisualizerService = uiVisualizerService;
-            _messageService = messageService;
+            this.uiVisualizerService = uiVisualizerService;
+            this.messageService = messageService;
 
             AddPerson = new TaskCommand(OnAddPersonExecuteAsync);
             EditPerson = new TaskCommand(OnEditPersonExecuteAsync, OnEditPersonCanExecute);
@@ -32,6 +32,7 @@ namespace WPF.GettingStarted.ViewModels
 
         /// <summary>
         /// Gets the family.
+        /// Получает семью.
         /// </summary>
         [Model]
         public Family Family
@@ -42,11 +43,13 @@ namespace WPF.GettingStarted.ViewModels
 
         /// <summary>
         /// Register the Family property so it is known in the class.
+        /// Зарегистрируйте свойство Family так, чтобы оно было известно в классе.
         /// </summary>
         public static readonly PropertyData FamilyProperty = RegisterProperty("Family", typeof(Family), null);
 
         /// <summary>
         /// Gets the family members.
+        /// Получает членов семьи.
         /// </summary>
         [ViewModelToModel("Family")]
         public ObservableCollection<Person> Persons
@@ -57,11 +60,13 @@ namespace WPF.GettingStarted.ViewModels
 
         /// <summary>
         /// Register the Persons property so it is known in the class.
+        /// Зарегистрируйте xktyjd ctvmb, чтобы они были известна в классе.
         /// </summary>
         public static readonly PropertyData PersonsProperty = RegisterProperty("Persons", typeof(ObservableCollection<Person>), null);
 
         /// <summary>
         /// Gets or sets the family name.
+        /// Получает или задает фамилию.
         /// </summary>
         [ViewModelToModel("Family")]
         public string FamilyName
@@ -72,11 +77,13 @@ namespace WPF.GettingStarted.ViewModels
 
         /// <summary>
         /// Register the FamilyName property so it is known in the class.
+        /// Зарегистрируйте свойство FamilyName, чтобы оно было известно в классе.
         /// </summary>
         public static readonly PropertyData FamilyNameProperty = RegisterProperty("FamilyName", typeof(string));
 
         /// <summary>
         /// Gets or sets the selected person.
+        /// Получает или задает выбранного человека.
         /// </summary>
         public Person SelectedPerson
         {
@@ -86,16 +93,19 @@ namespace WPF.GettingStarted.ViewModels
 
         /// <summary>
         /// Register the SelectedPerson property so it is known in the class.
+        /// Зарегистрируйте свойство SelectedPerson, чтобы оно было известно в классе.
         /// </summary>
         public static readonly PropertyData SelectedPersonProperty = RegisterProperty("SelectedPerson", typeof(Person), null);
 
         /// <summary>
         /// Gets the AddPerson command.
+        /// Получает команду AddPerson.
         /// </summary>
         public TaskCommand AddPerson { get; private set; }
 
         /// <summary>
         /// Method to invoke when the AddPerson command is executed.
+        /// Метод вызова при выполнении команды AddPerson.
         /// </summary>
         private async Task OnAddPersonExecuteAsync()
         {
@@ -104,9 +114,12 @@ namespace WPF.GettingStarted.ViewModels
 
             // Note that we use the type factory here because it will automatically take care of any dependencies
             // that the PersonViewModel will add in the future
+            // Обратите внимание, что мы используем фабрику типов здесь,
+            // потому что она автоматически позаботится о любых зависимостях,
+            // которые PersonViewModel будет добавлять в будущем
             var typeFactory = this.GetTypeFactory();
             var personViewModel = typeFactory.CreateInstanceWithParametersAndAutoCompletion<PersonViewModel>(person);
-            if (await _uiVisualizerService.ShowDialogAsync(personViewModel) ?? false)
+            if (await uiVisualizerService.ShowDialogAsync(personViewModel) ?? false)
             {
                 Persons.Add(person);
             }
@@ -114,11 +127,13 @@ namespace WPF.GettingStarted.ViewModels
 
         /// <summary>
         /// Gets the EditPerson command.
+        /// Получает команду EditPerson.
         /// </summary>
         public TaskCommand EditPerson { get; private set; }
 
         /// <summary>
         /// Method to check whether the EditPerson command can be executed.
+        /// Метод проверки возможности выполнения команды EditPerson.
         /// </summary>
         /// <returns><c>true</c> if the command can be executed; otherwise <c>false</c></returns>
         private bool OnEditPersonCanExecute()
@@ -128,23 +143,29 @@ namespace WPF.GettingStarted.ViewModels
 
         /// <summary>
         /// Method to invoke when the EditPerson command is executed.
+        /// Способ вызова, когда выполняется команда EditPerson.
         /// </summary>
         private async Task OnEditPersonExecuteAsync()
         {
             // Note that we use the type factory here because it will automatically take care of any dependencies
             // that the PersonViewModel will add in the future
+            // Обратите внимание, что мы используем фабрику типов здесь,
+            // потому что она автоматически позаботится о любых зависимостях,
+            // которые PersonViewModel будет добавлять в будущем
             var typeFactory = this.GetTypeFactory();
             var personViewModel = typeFactory.CreateInstanceWithParametersAndAutoCompletion<PersonViewModel>(SelectedPerson);
-            await _uiVisualizerService.ShowDialogAsync(personViewModel);
+            await uiVisualizerService.ShowDialogAsync(personViewModel);
         }
 
         /// <summary>
         /// Gets the RemovePerson command.
+        /// Получает команду RemovePerson.
         /// </summary>
         public TaskCommand RemovePerson { get; private set; }
 
         /// <summary>
         /// Method to check whether the RemovePerson command can be executed.
+        /// Способ проверки выполнения команды RemovePerson.
         /// </summary>
         /// <returns><c>true</c> if the command can be executed; otherwise <c>false</c></returns>
         private bool OnRemovePersonCanExecute()
@@ -154,10 +175,11 @@ namespace WPF.GettingStarted.ViewModels
 
         /// <summary>
         /// Method to invoke when the RemovePerson command is executed.
+        /// Способ вызова, когда выполняется команда RemovePerson.
         /// </summary>
         private async Task OnRemovePersonExecuteAsync()
         {
-            if (await _messageService.ShowAsync(string.Format("Are you sure you want to delete the person '{0}'?", SelectedPerson), 
+            if (await messageService.ShowAsync(string.Format("Are you sure you want to delete the person '{0}'?", SelectedPerson), 
                 "Are you sure?", MessageButton.YesNo, MessageImage.Question) == MessageResult.Yes)
             {
                 Persons.Remove(SelectedPerson);
